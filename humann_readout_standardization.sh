b65a1d924ea0039b5241fabcb436e25cecb2f7a0
lynchlab-ucsf <lynchlabequipment@gmail.com>
2021-03-03 08:55:34 -0800

    Initial Pipeline Commit

---
+++ humann_readout_standardization.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#$ -cwd
+#$ -l mem_free=1G
+#$ -l h_rt=12:00:00
+## #$ -m e
+## #$ -M kathryn.mccauley@ucsf.edu
+
+
+software_location=/wynton/group/lynch/kmccauley/mySoftware/
+## Note: first time this was run, three samples were still outstanding
+FASTA_DIRECTORY=/wynton/group/lynch/kmccauley/Metagenomics_CREW/dyad_mapping_v2/
+
+
+####
+cd $FASTA_DIRECTORY
+
+mkdir -p humann3_combined
+for i in */humann2_results/*genefamilies.tsv; do
+id=$(echo $i | cut -d '/' -f1)
+cp $i humann3_combined/${id}_genefamilies.tsv
+done
+
+for i in */humann2_results/*pathabundance.tsv; do
+id=$(echo $i | cut -d '/' -f1)
+cp $i humann3_combined/${id}_pathabundance.tsv
+done
+
+singularity exec -B $PWD:$PWD $software_location/humann.img humann_join_tables -i humann3_combined -o combined_genefamilies.tsv --file_name genefamilies
+singularity exec -B $PWD:$PWD $software_location/humann.img humann_renorm_table -i combined_genefamilies.tsv -o combined_genefamilies-cpm.tsv --units cpm --update-snames
+singularity exec -B $PWD:$PWD,/wynton/group/lynch/Shared/humann_db/utility_mapping/:/mnt $software_location/humann.img humann_regroup_table -i combined_genefamilies-cpm.tsv -o level4ec-cpm.tsv -c /mnt/map_level4ec_uniref90.txt.gz
+singularity exec -B $PWD:$PWD $software_location/humann.img humann_rename_table -i level4ec-cpm.tsv -o level4ec-cpm-named.tsv --names ec
+
+singularity exec -B $PWD:$PWD $software_location/humann.img humann_join_tables -i humann3_combined -o combined_pathabundance.tsv --file_name pathabundance
+singularity exec -B $PWD:$PWD $software_location/humann.img humann_renorm_table -i combined_pathabundance.tsv -o combined_pathabundance-cpm.tsv --units cpm --update-snames
+
---
+++ reads_based_data_generation.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+#$ -cwd
+#$ -pe smp 5
+#$ -l mem_free=10G
+#$ -l h_rt=80:00:00
+#$ -t 1-5
+## #$ -m e
+## #$ -M kathryn.mccauley@ucsf.edu
+
+## Where is your QC results directory?
+files_dir=/wynton/group/lynch/kmccauley/URECA_metagenomics/
+## What is the name of the specific directory?
+results_dir=metagenomics_results_113481
+
+
+
+######## No edits needed beyond this point
+software_location=/wynton/group/lynch/kmccauley/mySoftware/
+
+cd $files_dir
+sample1=$(echo `ls $results_dir` | cut -d " " -f $SGE_TASK_ID)
+
+echo "Processed Sample is:" $sample1
+
+## Jump straight into Kraken
+
+db_location=/wynton/group/lynch/Shared/kraken2_db/
+current_dir=`pwd`
+if [[ -z "$TMPDIR" ]]; then
+  if [[ -d /scratch ]]; then TMPDIR=/scratch/$USER; else TMPDIR=/tmp/$USER; fi
+    mkdir -p "$TMPDIR"
+    export TMPDIR
+fi
+
+## run kraken providing the paired reads
+cp $results_dir/$sample1/bbduk_ReadCleaning_Output/HumanFiltering/${sample1}_R1_clean_001.fastq.gz $TMPDIR
+cp $results_dir/$sample1/bbduk_ReadCleaning_Output/HumanFiltering/${sample1}_R2_clean_001.fastq.gz $TMPDIR
+
+cd $TMPDIR
+
+## kraken runs, but it's not saving the actual output
+singularity exec -B $PWD:$PWD,$db_location:/mnt ${software_location}kraken2.img kraken2 --db /mnt/ --report ${sample1}_kraken_report.txt --use-names --gzip-compressed --confidence 0.95 --memory-mapping --paired --threads $NSLOTS ${sample1}_R1_clean_001.fastq.gz ${sample1}_R2_clean_001.fastq.gz > ${sample1}_kraken_output.txt
+
+## Run Humann3 using catted reads, so cat reads first:
+cat ${sample1}_R1_clean_001.fastq.gz ${sample1}_R2_clean_001.fastq.gz > ${sample1}_clean_001.fastq.gz
+
+## then run humann3
+singularity exec -B $PWD:$PWD,/wynton/group/lynch/Shared/humann_db/metaphlan:/metaphlan,/wynton/group/lynch/Shared/humann_db/chocophlan:/chocophlan,/wynton/group/lynch/Shared/humann_db/uniref:/uniref $software_location/humann.img humann -i ${sample1}_clean_001.fastq.gz -o humann3_results --metaphlan-options "--bowtie2db /metaphlan" --nucleotide-database /chocophlan --protein-database /uniref --threads $NSLOTS
+
+rm *.gz
+rm -r humann3_results/*_temp
+
+mkdir -p $current_dir/kraken2_humann3_results/${sample1}
+cp -r $TMPDIR/* $current_dir/kraken2_humann3_results/${sample1}
+
